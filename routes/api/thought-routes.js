a0deb6e9fd40a9259b4d18a44baa33deec666af1
park-d <54589582+park-d@users.noreply.github.com>
2022-10-24 18:15:49 -0500

    added in thought routes connections

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getThoughtsbyID,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getThoughtsbyID).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction)
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
